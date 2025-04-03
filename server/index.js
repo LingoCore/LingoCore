5dc9c2715b1113b08c6f252abb9ed15b1c9998a6
Deniz Tunç <49831545+creeperkafasi@users.noreply.github.com>
2025-04-03 11:01:27 +0300

    Testlogin endpoint

--- server/index.js
+++ server/index.js
@@ -10,6 +10,35 @@ const PORT = 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
+
+/**
+ * @typedef { Object } User
+ * @property {string} userId
+ * @property {string} username
+ * @property {string} email
+ * @property {[string]} courses
+ */
+
+// Fake Database (Örnek veriler)
+/**
+ * @type [User]
+ */
+let users = [
+  { id: '0', username: 'deniztunc', email: 'deniz@example.com', courses: ["english"] },
+  { id: '1', username: 'emrecamuz', email: 'emre@example.com', courses: ["english"] },
+  { id: '2', username: 'egeyolsal', email: 'ege@example.com', courses: ["english"] }
+];
+
+// Temporary implementation without any auth
+app.post("/api/auth/testlogin", (req, res) => {
+  const { id } = req.body;
+  const selectedUser = users.find(user => user.id === id);
+  if (!selectedUser) {
+    res.status(401).json({ message: "User id not found." }).send();
+  }
+  res.status(200).json({ message: "Login successfull", data: selectedUser }).send();
+})
+
 // Sunucuyu Başlat
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
