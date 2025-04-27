54c8721032ec5e743aa0eacf5afa1fe789ffc7a6
Emre Camuzoğlu <emrecamuz06@gmail.com>
2025-04-27 13:59:55 +0300

    lessons page linked to course page

--- lingocore/lib/screens/course_screen.dart
+++ lingocore/lib/screens/course_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lingocore/lesson_pages.dart';
 
 class CourseScreen extends StatefulWidget {
   const CourseScreen({super.key});
@@ -76,7 +77,7 @@ class _CourseScreenState extends State<CourseScreen> {
                       Navigator.push(
                         context, 
                         MaterialPageRoute(
-                          builder: (context) => LessonDetailPage(lessonTitle: lessonTitle),
+                          builder: (context) => LessonPages(),
                         )
                       );
                     },
@@ -86,33 +87,6 @@ class _CourseScreenState extends State<CourseScreen> {
             ),
           );
         }
-      ),
-    );
-  }
-}
-
-
-class LessonDetailPage extends StatelessWidget {
-  
-  // lesson title from previous page
-  final String lessonTitle; 
-
-  // Constructor: Requires the course title to be retrieved when creating this page
-  const LessonDetailPage({Key? key, required this.lessonTitle}) : super(key: key);
-  
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(lessonTitle),
-      ),
-      body: Center(
-
-        child: Text(
-          'Bu sayfa "$lessonTitle" içeriğini gösterecektir.\n\nBuraya video, metin, alıştırmalar vb. ekleyebilirsiniz.',
-          style: TextStyle(fontSize: 19.0),
-          textAlign: TextAlign.center,
-        ),
       ),
     );
   }
