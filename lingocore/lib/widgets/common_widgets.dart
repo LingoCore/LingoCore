dd323821399dbefeea3f0382843b25431a103791
Emre Camuzoğlu <emrecamuz06@gmail.com>
2025-04-27 18:12:11 +0300

    Created a separate dart files for question widgets and for other widgets too

---
+++ lingocore/lib/widgets/common_widgets.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+class ButtonWidget extends StatelessWidget {
+  final String text;
+  final Function() buttonFunction;
+  ButtonWidget({super.key, required this.text, required this.buttonFunction});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.greenAccent, Colors.white],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            padding: EdgeInsets.symmetric(vertical: 30, horizontal: 50),
+            elevation: 0,
+            shadowColor: Colors.transparent,
+            backgroundColor: Colors.transparent,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+              side: BorderSide(color: Colors.green, width: 3),
+            ),
+          ),
+          onPressed: buttonFunction,
+          child: Text(text, style: TextStyle(fontSize: 30)),
+        ),
+      ),
+    );
+  }
+}
---
+++ lingocore/lib/widgets/questions.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:lingocore/lesson_pages.dart';
+import 'package:lingocore/widgets/common_widgets.dart';
+
+class TrueFalseQuestion extends StatelessWidget {
+  final Question question;
+  final Function(String) callback;
+  TrueFalseQuestion({super.key, required this.question, required this.callback});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(100.0),
+          child: Text(question.questionText),
+        ),
+
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ButtonWidget(text: 'Doğru', buttonFunction: () => callback('Şık 1')),
+            ButtonWidget(text: 'Yanlış', buttonFunction: () => callback('Yanlış')),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class MultipleChoiceQuestion extends StatelessWidget {
+  final Question question;
+  final Function(String) callback;
+  MultipleChoiceQuestion({super.key, required this.question, required this.callback});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(50.0),
+          child: Text(question.questionText),
+        ),
+        Column(
+          children:
+              question.options
+                  ?.map(
+                    (option) =>
+                        ButtonWidget(text: option, buttonFunction: () => callback(option)),
+                  )
+                  .toList() ??
+              [],
+        ),
+      ],
+    );
+  }
+}
