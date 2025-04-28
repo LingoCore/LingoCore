87e75badd923a7611d14da7e694fdd10f10a39ca
Deniz Tunç <49831545+creeperkafasi@users.noreply.github.com>
2025-04-28 13:14:24 +0300

    Fix the login ui to the bottom of the page

--- lingocore/lib/login_page.dart
+++ lingocore/lib/login_page.dart
@@ -9,76 +9,82 @@ class LoginPage extends StatelessWidget {
     // ekran yükekliğini al ve belli bi orana dönüştür
 
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Column(
-            children: [
-              Icon(Icons.account_circle_outlined, size: 100),
-              Text(
-                "Giriş Ekranı",
-                style: TextStyle(color: Colors.black, fontSize: 20),
-              ),
-            ],
-          ),
-
-          Material(
-            color: Colors.grey[300],
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: InkWell(
-              splashFactory: InkRipple.splashFactory,
-              borderRadius: BorderRadius.circular(10),
-              onTap:
-                  () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => HomePage(),),
-                  ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 6,
+      body: Padding(
+        padding: EdgeInsets.only(
+          top: MediaQuery.of(context).size.height * 0.35,
+          bottom: 24,
+          left: 16,
+          right: 16,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                Icon(Icons.account_circle_outlined, size: 100),
+                Text(
+                  "Giriş Ekranı",
+                  style: TextStyle(color: Colors.black, fontSize: 20),
                 ),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset("assets/images/google.png", height: 50),
-                    SizedBox(width: 10),
-                    Text(
-                      "Google ile giriş yap",
-                      style: TextStyle(fontSize: 16),
+              ],
+            ),
+            Material(
+              color: Colors.grey[300],
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: InkWell(
+                splashFactory: InkRipple.splashFactory,
+                borderRadius: BorderRadius.circular(10),
+                onTap:
+                    () => Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => HomePage()),
                     ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 6,
+                  ),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("assets/images/google.png", height: 50),
+                      SizedBox(width: 10),
+                      Text(
+                        "Google ile giriş yap",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Column(
+              // bottom section locked at bottom by spaceBetween
+              children: [
+                Row(
+                  children: [
+                    blackDivider(),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Text(
+                        "© LingoCore - 2025",
+                        style: TextStyle(color: Colors.black, fontSize: 16),
+                      ),
+                    ),
+                    blackDivider(),
                   ],
                 ),
-              ),
+                Text(
+                  "Tüm Hakları Saklıdır.",
+                  style: TextStyle(color: Colors.grey, fontSize: 16),
+                ),
+              ],
             ),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Row(
-                children: [
-                  blackDivider(),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: Text(
-                      "© LingoCore - 2025",
-                      style: TextStyle(color: Colors.black, fontSize: 16),
-                    ),
-                  ),
-                  blackDivider(),
-                ],
-              ),
-              Text(
-                "Tüm Hakları Saklıdır.",
-                style: TextStyle(color: Colors.grey, fontSize: 16),
-              ),
-            ],
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
